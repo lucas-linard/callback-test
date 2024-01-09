dd7dac957fdd8584c22afe60a1dafe542e3b8907
LucasAlencar <lucas.linard@outlook.com>
2024-01-09 18:11:25 -0300

    feat: rates call to Giross API

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.post("/", function (req, res, next) {
-  console.log(JSON.stringify(req.body, null, 2));
+router.post("/", async function (req, res, next) {
 
+  const rate = await getRatesByGiross(req.body.rate.origin, req.body.rate.destination);
+  console.log("rate", rate);
   let dataAtual = new Date();
   let doisDiasNoFuturo = new Date(dataAtual);
   doisDiasNoFuturo.setDate(dataAtual.getDate() + 2);
@@ -20,9 +21,28 @@ router.post("/", function (req, res, next) {
         phone_required: true,
         min_delivery_date: new Date(),
         max_delivery_date: doisDiasNoFuturo,
-      }
-    ]
+      },
+    ],
   });
 });
+
+async function getRatesByGiross(origin, destination) {
+  console.log("origin", `${origin.address1} ${origin.address2} ${origin.city}`);
+  console.log(
+    "destination",
+    `${destination.address1} ${destination.address2} ${destination.city}`
+  );
+  fetch("https://teste.giross.com.br/api/empresa/estimated-value", {
+    method: "POST",
+    body: {
+      origem: {
+        address_string: `${origin.address1} ${origin.address2} ${origin.city}`,
+      },
+      destino: {
+        address_string: `${destination.address1} ${destination.address2} ${destination.city}`,
+      },
+    },
+  }).then((res) => res.json());
+}
 
 module.exports = router;
